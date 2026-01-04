a0212951178db5f0a78160d4ade7ab9950ca2372
Codecoo <tallentaanugerah@proton.me>
2026-01-04 13:13:18 +0700

    change target framework to netstandard2.0

--- csharp/Services/ServerStartup.cs
+++ csharp/Services/ServerStartup.cs
@@ -16,52 +16,54 @@ namespace FlutterSharpRpc.Services
             CancellationToken cancellationToken = default)
             where TServer : class
         {
-            await using var stdin = Console.OpenStandardInput();
-            await using var stdout = Console.OpenStandardOutput();
+            using (var stdin = Console.OpenStandardInput())
+            using (var stdout = Console.OpenStandardOutput())
 
-            var duplex = FullDuplexStream.Splice(stdin, stdout);
-
-            formatter.JsonSerializerOptions.PropertyNamingPolicy =
-                JsonNamingPolicy.CamelCase;
-
-            formatter.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-
-            using var handler = new HeaderDelimitedMessageHandler(
-                duplex,
-                duplex,
-                formatter);
-
-            using var rpc = new JsonRpc(handler);
-
-            if (server is IRpcNotifierAware rpcNotifierAware)
+            using (var duplex = FullDuplexStream.Splice(stdin, stdout))
             {
-                rpcNotifierAware.AttachNotifier(new RpcNotifier(rpc));
-            }
+                formatter.JsonSerializerOptions.PropertyNamingPolicy =
+                   JsonNamingPolicy.CamelCase;
 
-            // If we dont set registerMethods it means we are using normal mode instead of aot
-            if (registerMethods == default)
-            {
-                rpc.AddLocalRpcTarget(server);
-                RpcLog.Info($"RPC target registered: {typeof(TServer).FullName}");
-            }
-            else
-            {
-                await registerMethods(rpc, server);
-                RpcLog.Info($"RPC target registered: {typeof(TServer).FullName}");
-            }
+                formatter.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
 
-            rpc.Disconnected += (_, e) => RpcLog.Warn($"RPC disconnected: {e.Reason}");
+                using (var handler = new HeaderDelimitedMessageHandler(
+                    duplex,
+                    duplex,
+                    formatter))
 
-            rpc.StartListening();
-            RpcLog.Info("csharp-json-rpc server started. Waiting for requests...");
+                using (var rpc = new JsonRpc(handler))
+                {
+                    if (server is IRpcNotifierAware rpcNotifierAware)
+                    {
+                        rpcNotifierAware.AttachNotifier(new RpcNotifier(rpc));
+                    }
 
-            using (cancellationToken.Register(() =>
-            {
-                RpcLog.Warn("Cancellation requested, disposing RPC");
-                rpc.Dispose();
-            }))
-            {
-                await rpc.Completion.ConfigureAwait(false);
+                    // If we dont set registerMethods it means we are using normal mode instead of aot
+                    if (registerMethods == default)
+                    {
+                        rpc.AddLocalRpcTarget(server);
+                        RpcLog.Info($"RPC target registered: {typeof(TServer).FullName}");
+                    }
+                    else
+                    {
+                        await registerMethods(rpc, server);
+                        RpcLog.Info($"RPC target registered: {typeof(TServer).FullName}");
+                    }
+
+                    rpc.Disconnected += (_, e) => RpcLog.Warn($"RPC disconnected: {e.Reason}");
+
+                    rpc.StartListening();
+                    RpcLog.Info("csharp-json-rpc server started. Waiting for requests...");
+
+                    using (cancellationToken.Register(() =>
+                    {
+                        RpcLog.Warn("Cancellation requested, disposing RPC");
+                        rpc.Dispose();
+                    }))
+                    {
+                        await rpc.Completion.ConfigureAwait(false);
+                    }
+                }
             }
         }
     }
